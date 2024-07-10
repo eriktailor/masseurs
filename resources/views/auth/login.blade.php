2748804772477677e56af7287ab10307674371d5
Erik Tailor <erno22szabo@gmail.com>
2024-07-10 04:07:54 +0200

    login view updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,20 @@
-<form method="POST" action="{{ route('login') }}">
+@extends('layouts.auth')
+
+<div class="w-100 vh-100 d-flex align-items-center justify-content-center">
+<form class="col-sm-8 col-lg-3" method="POST" action="{{ route('login') }}">
     @csrf
-    <div>
-        <label for="email">Email</label>
-        <input id="email" type="email" name="email" required>
+    <div class="d-flex align-items-center mb-5">
+        <img class="me-3" src="{{ asset('/img/logos/logo_emblem.png') }}" alt="Saeng Lányok" width="50" height="50">
+        <h1 class="h1 mb-0">Saeng Lányok</h1>
     </div>
-    <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password" required>
+    <div class="mb-3">
+        <label class="form-label" for="email">Email</label>
+        <input class="form-control" id="email" type="email" name="email" required>
     </div>
-    <button type="submit">Login</button>
+    <div class="mb-4">
+        <label class="form-label" for="password">Password</label>
+        <input class="form-control" id="password" type="password" name="password" required>
+    </div>
+    <button class="w-100 btn btn-primary" type="submit">Belépés</button>
 </form>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,4 @@
-<!doctype html>
-<html lang="hu">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Masszőrök Adatai | Saeng Tian Thai Masszázs</title>
-<!--     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicons/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('assets/img/favicons/site.webmanifest') }}"> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap">
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-</head>
+@include('layouts.head')
 
 <body class="bg-light">
 
@@ -67,7 +48,7 @@
                             @csrf
                         </form>
                         <a class="dropdown-item" id="logoutButton" href="{{ route('logout') }}">
-                            Logout
+                            Kijelentkezés
                         </a>
                     </li>
                 </ul>
